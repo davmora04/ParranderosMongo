ada6ffc7509518f2e109f3f558e1e631e7f016be
silvanaecheverry <s.echeverrym@uniandes.edu.co>
2024-11-25 00:30:50 -0500

    crear categoria de producto

--- MiniParranderosNoSQL-master/demo/src/main/java/uniandes/edu/co/demo/repository/ProductoRepository.java
+++ MiniParranderosNoSQL-master/demo/src/main/java/uniandes/edu/co/demo/repository/ProductoRepository.java
@@ -1,7 +1,21 @@
 package uniandes.edu.co.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
 import uniandes.edu.co.demo.modelo.Producto;
 
 public interface ProductoRepository extends MongoRepository<Producto, Integer> {
+
+    // Buscar una categoría dentro de los productos usando el código
+    @Query("{ 'categoria.codigo': ?0 }")
+    List<Producto> findProductosByCategoriaCodigo(int codigo);
+
+    // Buscar una categoría dentro de los productos usando el nombre
+    @Query("{ 'categoria.nombre': ?0 }")
+    List<Producto> findProductosByCategoriaNombre(String nombre);
+
+
 }
